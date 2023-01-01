936066c2c6a8e1327448d486b066542e29b6c8bf
ngocnamk3er <ngocnamk3er@gmail.com>
2023-01-01 22:45:29 +0700

    generic view

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-   path('', views.list, name='blog'),
-   path('<int:id>/', views.post, name='postx'),
+   path('', views.ListPostView.as_view(), name='blog'),
+   path('<int:pk>/', views.PosteDetail.as_view(), name='post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,14 @@
+from django.views.generic import ListView, DetailView
 from django.http import Http404
 from django.shortcuts import render
 from .models import Post
 # Create your views here.
-def list(request):
-   Data = {'Posts': Post.objects.all().order_by('-date')}
-   return render(request, 'blog/blog.html', Data)
-def post(request, id):
-   post = Post.objects.get(id=id)
-   return render(request, 'blog/post.html', {'post': post})
+class ListPostView(ListView):
+   queryset = Post.objects.all().order_by('-date')
+   template_name = 'blog/blog.html'
+   context_object_name = 'Posts'
+   paginate_by = 1
+class PosteDetail(DetailView):
+    model = Post
+    template_name='blog/post.html'
 
